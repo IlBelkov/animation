aaaaaf364ec07f05b0508f1dc8d5b68e195a00fd
IlyaBel <ilia_b@softgrad.com>
2020-07-28 15:25:56 +0300

    added 4 animations

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1781,3 +1781,23 @@
 
 
 var s = skrollr.init();
+
+const buttons = document.querySelectorAll('.button1__btn');
+buttons.forEach(btn => {
+	btn.addEventListener('mouseenter', function(e){
+		let x = e.clientX - e.target.offsetLeft;
+		let y = e.clientY - e.target.offsetTop;
+
+		let ripples = document.createElement('span');
+		ripples.classList.add('button1__elem');
+		ripples.style.left = x + 'px';
+		ripples.style.right = x + 'px';
+		ripples.style.top = y + 'px';
+		ripples.style.bottom = y + 'px';
+		this.appendChild(ripples);
+
+		setTimeout(() => {
+			ripples.remove()
+		}, 1000);
+	})
+})
